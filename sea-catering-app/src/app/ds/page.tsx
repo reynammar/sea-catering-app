c7612d4efdd78fee19a65451e14ef37dfff3e2ac
Arif Ammar <ammarreynarif@gmail.com>
2025-07-01 17:04:43 +0700

    style: add custom css at global css

---
+++ sea-catering-app/src/app/ds/page.tsx
@@ -0,0 +1,12 @@
+import DesignSystemContainer from "@/feature/ds/container/DesignSystemContainer";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <DesignContainer />
+    </>
+  );
+};
+
+export default page;
---
+++ sea-catering-app/src/feature/ds/container/DesignSystemContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DesignSystemContainer = () => {
+    return (
+        <main className="mycontainer mt-30 flex flex-col gap-10">
+            
+        </main>
+    )
+}
